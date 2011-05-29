bfed000b26b050c5fd661d784c7108fbc0b6928b
Matt Holloway <matthew.holloway@logicwerx.com>
2011-05-29 15:34:36 -0400

    refactor: fixed tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
 describe PagesController do
-
+  render_views
   describe "GET 'home'" do
     it "should be successful" do
-      get 'home'
+      get :home
       response.should be_success
     end
     
@@ -17,12 +17,12 @@ describe PagesController do
 
   describe "GET 'contact'" do
     it "should be successful" do
-      get 'contact'
+      get :contact
       response.should be_success
     end
     
     it "should have the right title" do
-      get 'home'
+      get :contact
       response.should have_selector("title",
                       :content => "| Contact")
     end
@@ -30,12 +30,12 @@ describe PagesController do
   
   describe "GET 'about'" do
     it "should be successful" do
-      get 'about'
+      get :about
       response.should be_success
     end
     
     it "should have the right title" do
-      get 'home'
+      get :about
       response.should have_selector("title",
                       :content => "| About")
     end
