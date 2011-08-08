eec64f5029c856c9f566d51388b51236cfbb27ed
Matt Holloway <matthew.holloway@logicwerx.com>
2011-08-07 20:26:37 -0400

    User model with passwords, and new user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                   "Cinder Chops"
+  user.email                  "cinder@chops.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
