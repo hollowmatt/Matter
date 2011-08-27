600b50fbd7ca31b5d50662b26bab2f4a43d3f355
Matt Holloway <matthew.holloway@logicwerx.com>
2011-08-27 17:05:47 -0400

    Done sign in and out

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -58,4 +58,12 @@ describe SessionsController do
       end
     end
   end
+  
+  describe "DELETE 'destroy'" do
+    it "should sign a user out" do
+      test_sign_in(Factory(:user))
+      delete :destroy
+      response.should redirect_to(root_path)
+    end
+  end
 end
