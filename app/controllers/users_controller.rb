b2ed983e2e527d9aceda10b30498db13805183ca
Matt Holloway <matthew.holloway@logicwerx.com>
2011-09-05 15:58:06 -0400

    Done with user edit/update - one issue on route and refresh to fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   before_filter :authenticate, :only => [:index, :edit, :update]
   before_filter :correct_user, :only => [:edit, :update]
+  before_filter :admin_user, :only => :destroy
   
   def index
     @title = "All users"
-    @users = User.all
+    @users = User.paginate(:page => params[:page], :per_page => 10)
   end
   
   def new
@@ -43,6 +44,13 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    @destroyed = User.find(params[:id])
+    User.find(params[:id]).destroy
+    flash[:success] = @destroyed.name + " has been destroyed"
+    redirect_to users_path
+  end
+  
   private
     
     def authenticate
@@ -54,4 +62,7 @@ class UsersController < ApplicationController
       redirect_to(root_path) unless current_user?(@user)
     end
     
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
